e38247c17b5565b5064bf6128f58a33df533a758
davis wafula <42667789+Davisdavy@users.noreply.github.com>
2021-04-14 19:27:08 +0300

    Update repository.dart

--- lib/services/repository.dart
+++ lib/services/repository.dart
@@ -5,8 +5,9 @@ import 'package:xtranet/model/weather.dart';
 
 class WeatherRepo{
   Future<Weather> getWeather(String city) async{
+    //APIKEYto be hidden in the future
     final result = await http.Client().get(Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=f141d46ca55c3c286d68a00b38a94856"));
-
+    //Check connect state
     if(result.statusCode != 200)
       throw Exception();
     else
